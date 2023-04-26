2e437aa9c051dedad3c941c442cf0de3c81de4f3
Isma Navarro <ismanapa@gmail.com>
2023-04-26 11:09:42 +0200

    feat: chapter 1 - step 3
    Co-authored-by: Ronny Ancorini <ronny.ancorini@codely.com>

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -3,3 +3,22 @@
 
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
+
+function checkEmotion(knownEmotions, emotion) {
+	// Simulate database processing time by waiting a second...
+	return new Promise((resolve) =>
+		setTimeout(() => {
+			resolve(knownEmotions.has(emotion));
+		}, 1000)
+	);
+}
+
+async function speak(knownEmotions, newEmotion, phrase) {
+	const hasEmotion = await checkEmotion(knownEmotions, newEmotion);
+
+	if (hasEmotion) {
+		return `"${phrase}" (${newEmotion})`;
+	} else {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+}
