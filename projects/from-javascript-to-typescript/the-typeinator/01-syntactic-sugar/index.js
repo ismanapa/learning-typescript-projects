9278441dea077d656c1959fa88cae5a0dc91da21
Isma Navarro <ismanapa@gmail.com>
2023-04-26 10:51:11 +0200

    feat: chapter 1 - step 1
    Co-authored-by: Ronny Ancorini <ronny.ancorini@codely.com>

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -2,3 +2,21 @@
 // See ./original.js for its older JavaScript code.
 
 module.exports.announceMachines = announceMachines;
+
+function announceMachines(announce, ...machines) {
+	let label;
+	let labelsCount = 0;
+
+	for (let machine of machines) {
+		if (machine.label) {
+			label = machine.label;
+			labelsCount += 1;
+		} else {
+			label = `Make: ${machine.make}; Model: ${machine.model}`;
+		}
+
+		announce(label);
+	}
+
+	return labelsCount;
+}
