15e375a2dcf785bf37a46632e0e169978d31a98d
Isma Navarro <ismanapa@gmail.com>
2023-04-26 12:24:59 +0200

    chapter 4 - step 1
    Co-authored-by: Ronny Ancorini <ronny.ancorini@codely.com>

--- projects/objects/various-lawyerings/01-aviary-classification/index.ts
+++ projects/objects/various-lawyerings/01-aviary-classification/index.ts
@@ -1,4 +1,11 @@
 // Write your Bird type here! ✨
+interface Bird {
+	dangerous?: boolean;
+	diet: "omnivore" | "carnivore";
+	name: string;
+	noisy?: boolean;
+	intelligent?: boolean;
+}
 
 export const birds: Bird[] = [
 	{
