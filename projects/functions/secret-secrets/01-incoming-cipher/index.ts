b3510b1a98c8fb7a543d608d7c59ade7c8674017
Ronny Ancorini <ronnyancorini@gmail.com>
2023-05-03 16:30:59 +0200

    feat: chapter 5 - step 2 Co-authored-by: Isma Navarro <ismanapa@gmail.com>

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,29 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+export type Cipher = (character: string) => string;
+
+export function createCipher(cipher: Cipher) {
+	return (text: string) => {
+		let secret = "";
+		for (const character of text) {
+			secret += cipher(character);
+		}
+		return secret;
+	};
+}
+
+const createCipher2 = (cipher: Cipher) => (text: string) => {
+	let secret = "";
+	for (const character of text) {
+		secret += cipher(character);
+	}
+	return secret;
+};
+
+createCipher2(() => "")();
+
+const test = (parametro: string) => {
+	return parametro + 1;
+};
+
+const test2 = (parametro: string) => parametro + 1;
--- projects/functions/secret-secrets/02-dr-on/index.ts
+++ projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,13 @@
 // Write your createAdvancedCipher function here! ✨
 // You'll need to export it so the tests can run it.
+export type Cipher = (character: string) => string;
+
+export function createAdvancedCipher(
+	onVowel: Cipher,
+	onConsonant: Cipher,
+	onPunctuation: Cipher
+) {
+	return (text: string) => {
+		return "";
+	};
+}
